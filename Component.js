dc9c7600d75bc3ce2df4739ccc675e6f92f46192
canery2 <canery2@users.noreply.github.com>
2025-08-29 13:19:01 +0000

    deploy: 3165c5c460b0fb21810312071348dd9be34627c8

---
+++ Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/UIComponent","sap/ui/model/json/JSONModel"],(e,t)=>{"use strict";return e.extend("sample-app.Component",{metadata:{manifest:"json"},init:function(){e.prototype.init.apply(this,arguments);const n={recipient:{name:"World"}};const o=new t(n);this.setModel(o)}})});
+//# sourceMappingURL=Component.js.map
---
+++ controller/App.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/Device","sap/ui/core/mvc/Controller","sap/ui/model/Filter","sap/ui/model/FilterOperator","sap/ui/model/json/JSONModel","sap/base/strings/formatMessage","sap/m/MessageToast"],(e,s,a,p,r,o,i)=>{"use strict";return s.extend("sample-app.controller.App",{})});
+//# sourceMappingURL=App.controller.js.map
