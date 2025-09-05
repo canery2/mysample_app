36c353eb7cf2c4b2375d0b42b23a239b80e1c711
canery2 <canery2@users.noreply.github.com>
2025-09-05 14:15:27 +0000

    deploy: 418e6ea0677a5cfad384b16e1ed1e41c67be9737

--- resources/sap/ui/Global-dbg.js
+++ resources/sap/ui/Global-dbg.js
@@ -61,7 +61,7 @@ sap.ui.define([
 		 */
 		version: "1.139.0",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20250905-1333" }
+		buildinfo : { lastchange : "", buildtime : "20250905-1414" }
 	};
 
 	/**
